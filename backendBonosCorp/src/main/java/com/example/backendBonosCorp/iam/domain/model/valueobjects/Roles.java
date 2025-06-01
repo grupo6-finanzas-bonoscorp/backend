63b0d270b969d3405fe809b411db965ff299aaca
BarbaraQP15 <u202211800@upc.edu.pe>
2025-06-01 01:21:17 -0500

    fix: roles updated (ROLE_ANALYST)

--- backendBonosCorp/src/main/java/com/example/backendBonosCorp/iam/domain/model/valueobjects/Roles.java
+++ backendBonosCorp/src/main/java/com/example/backendBonosCorp/iam/domain/model/valueobjects/Roles.java
@@ -1,7 +1,7 @@
 package com.example.backendBonosCorp.iam.domain.model.valueobjects;
 
 public enum Roles {
-    ROLE_SUPER_ADMIN,
     ROLE_ADMIN,
-    ROLE_USER
+    ROLE_USER,
+    ROLE_ANALYST,
 }
